759548b09f6d966ee618e9b786231b957027d574
Jide Olaniyan <olaniyanba@gmail.com>
2024-10-21 15:13:54 +0100

    Updated config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/PixelPerfect" : "/PixelPerfect",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/PixelPerfect" : "/PixelPerfect",
   images: {
     unoptimized: true,
   },

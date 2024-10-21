64a1be786a6e37cdfaab61350e7c6a5ac16b749d
Mohammed Laniyan <mohammedlaniyan@gmail.com>
2024-10-21 18:42:58 +0100

    logo-update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import ThemeToggle from "@/components/themeToggle/ThemeToggle";
+// import ThemeToggle from "@/components/themeToggle/ThemeToggle";
 import { MdOutlineMenu } from "react-icons/md";
 import Image from "next/image";
 import Link from "next/link";
-import Logo_light from "../../../public/images/pixelperfect_blue_logo.webp";
-import Logo_dark from "../../../public/images/pixelperfect_white_logo.webp";
+import logoSymbol from "../../../public/SVG/logo-symbol.svg";
+import logoText from "../../../public/SVG/logo-text.svg";
 import { usePathname } from "next/navigation";
 import SideMenu from "./sideMenu/SideMenu";
 import { useTheme } from "next-themes";
@@ -41,15 +41,21 @@ const Header = () => {
 
   return (
     <nav
-      className={`h-20 w-full flex fixed top-0 justify-between items-center px-4 md:px-10 z-50 ${
+      className={`h-16 w-full flex fixed top-0 justify-between items-center px-4 md:px-10 z-50 ${
         header && "bg-[#171717] opacity-95 transition ease-in-out"
       }`}>
-      <div className="w-16 h-16 relative">
+      <div className="flex gap-4 relative">
         <Image
-          src={resolvedTheme === "dark" ? Logo_dark : Logo_light}
+          src={logoSymbol}
           alt="logo"
           priority
-          className="object-contain object-center w-full h-full"
+          className="object-contain object-center w-10 h-10 text-yellow-400"
+        />
+        <Image
+          src={logoText}
+          alt="logo"
+          priority
+          className="object-contain object-center w-32"
         />
       </div>
       <div className="h-full flex items-center justify-center gap-2">
@@ -60,7 +66,7 @@ const Header = () => {
               href={item.url}
               className={`${
                 pathname === item.url && "text-indigo-400"
-              } active:scale-95 capitalize text-sm font-semibold hover:bg-neutral-200 dark:hover:bg-slate-800 hover:bg-opacity-20 hover:text-indigo-400 p-2 rounded-md`}>
+              } active:scale-95 capitalize text-sm font-semibold hover:bg-neutral-800 hover:bg-opacity-40 hover:text-indigo-400 p-2 rounded-md`}>
               {item.title}
             </Link>
           ))}

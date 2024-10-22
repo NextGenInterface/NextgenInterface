61e773cd1f3fa1ea62fbd2abef19c5cacb34208c
Mohammed Laniyan <mohammedlaniyan@gmail.com>
2024-10-22 10:08:01 +0100

    qwery clean up

--- src/components/header/sideMenu/SideMenu.tsx
+++ src/components/header/sideMenu/SideMenu.tsx
@@ -20,40 +20,44 @@ const Menu = ({
     <div
       className={`${
         !menuNav ? "hidden" : "visible"
-      } w-full h-full px-4 fixed top-0 left-0 bg-light-background dark:bg-dark-background z-40 dark:bg-opacity-100 shadow-md`}>
-      <div className="flex justify-between items-center pt-6">
-        <div className="flex gap-4 relative">
-          <Image
-            src={logoSymbol}
-            alt="logo"
-            priority
-            className="object-contain object-center w-10 h-10 text-yellow-400"
-          />
-          <Image
-            src={logoText}
-            alt="logo"
-            priority
-            className="object-contain object-center w-32"
-          />
+      } w-full h-full fixed top-0 left-0 z-40 bg-background `}>
+      <div className="h-16 w-full flex fixed top-0 justify-between items-center px-6 z-50">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-1 relative">
+            <Image
+              src={logoSymbol}
+              alt="logo"
+              priority
+              className="object-contain object-center w-8 h-8"
+            />
+            <Image
+              src={logoText}
+              alt="logo"
+              priority
+              className="object-contain object-center w-32"
+            />
+          </div>
         </div>
         <IoMdClose
           onClick={() => setMenuNav(false)}
-          className="text-3xl relative -top-1"
+          className="text-3xl relative "
         />
       </div>
-      <div className="w-8 z-50"></div>
-      <div className="flex flex-col py-6 h-full">
-        {menu.map((item: any, idx: number) => (
-          <Link
-            key={idx}
-            onClick={() => setMenuNav(false)}
-            href={item.url}
-            className={`${
-              pathname === item.url && "text-indigo-400"
-            } capitalize text-md font-semibold hover:bg-neutral-200 dark:hover:bg-slate-800 hover:bg-opacity-20 hover:text-indigo-400 py-4`}>
-            {item.title}
-          </Link>
-        ))}
+
+      <div className="p-6 mt-12">
+        <div className="flex flex-col py-6 h-full">
+          {menu.map((item: any, idx: number) => (
+            <Link
+              key={idx}
+              onClick={() => setMenuNav(false)}
+              href={item.url}
+              className={`${
+                pathname === item.url && "text-indigo-400"
+              } capitalize text-md font-semibold hover:bg-neutral-200 dark:hover:bg-slate-800 hover:bg-opacity-20 hover:text-indigo-400 py-4`}>
+              {item.title}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/utils/pageLayout/PageLayout.tsx
+++ src/components/utils/pageLayout/PageLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
-  return <section className="px-4 md:mx-20">{children}</section>;
+  return <section className="mx-6 mt-24 relative">{children}</section>;
 };
 
 export default PageLayout;
